8e48c91192483293b3eafb748578e53abfcf44cd
Dominik <gradiskid@gmail.com>
2022-06-21 23:58:16 +0200

    added introductory/info part

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello, <br />
+        My name is Dominik Gradiški
+      </SectionTitle>
+      <SectionText>
+        The purpose of JS Mastery is to help aspiring. True web development skills offered to bring to every customer and bring value to the world of web development.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
