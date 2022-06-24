4d6369a8ac3fa9383a769a583fae7409f1fed12c
Dominik <gradiskid@gmail.com>
2022-06-24 23:54:04 +0200

    modified projects into constants & project cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,31 +4,25 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
-const projectsDemo = [{
-  title: 'Project 1',
-  description: 'This is a description for javascript project.'
-},{
-  title: 'Project 2',
-  description: 'This is a description for javascript project.'
-},{
-  title: 'Project 3',
-  description: 'This is a description for javascript project.'
-},{
-  title: 'Project 4',
-  description: 'This is a description for javascript project.'
-}];
+
 
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
       <SectionTitle main>Projects</SectionTitle>
       <GridContainer>
-        {projectsDemo.map((project) => (
-          <div>
-            {project.title}
-            <br/>
-            {project.description}
-          </div>
+        {projects.map(({ id, image, title, description, tags, source, visit}) => (
+          <BlogCard key={id}>
+            <Img src={image}/>
+            <TitleContent>
+              <HeaderThree title={title}>
+              <Hr />
+              </HeaderThree>
+            </TitleContent>
+            <CardInfo>
+              {description}
+            </CardInfo>
+          </BlogCard>
         ))}
       </GridContainer>
   </Section>
