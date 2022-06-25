867f232684413a55714843234ee27bc47f14d862
Dominik <gradiskid@gmail.com>
2022-06-25 19:11:33 +0200

    added item lists

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -2,11 +2,56 @@ import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import { SiFigma } from "react-icons/si";
+
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the web development world, same as application dev.
+      From Back-end to Front-end, mostly MERN stack.
+    </SectionText>
+    {/* react icons search for other icons */}
+    <List>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js
+          </ListParagraph>
+        </ListContainer>
+         
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Node.js
+            Databases
+          </ListParagraph>
+        </ListContainer>
+         
+      </ListItem>
+      <ListItem>
+        <SiFigma size="3rem"/>
+        <ListContainer>
+          <ListTitle>Design</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            tools like Figma
+          </ListParagraph>
+        </ListContainer>
+         
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
